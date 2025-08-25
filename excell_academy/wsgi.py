dcdf43260541d8ccd3b5ee92b43e1c6c6b01657f
Patrick <kyleasante4@gmail.com>
2025-08-25 15:23:47 +0000

    added dotenv and edited wsgi to point towards dev

--- excell_academy/wsgi.py
+++ excell_academy/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excell_academy.settings.prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'excell_academy.settings.dev')
 
 application = get_wsgi_application()
 
