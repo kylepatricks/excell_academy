7b55738f2404b9325d4a99f78c0f8cd76dcb95d5
Patrick <kyleasante4@gmail.com>
2025-09-03 04:20:32 +0000

    fix redirects

--- excell_academy/settings/common.py
+++ excell_academy/settings/common.py
@@ -72,16 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'excell_academy.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 
 # Password validation
